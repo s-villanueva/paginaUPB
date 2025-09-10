1f9a2c16e1ce1ba961c244f276514be579a4dcb8
Santiago Villanueva <santiagovillanueva710@gmail.com>
2025-09-10 19:15:07 -0400

    Fourth commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // index.js
 
+const menuBtn = document.getElementById("menu-btn");
+    const sidebar = document.getElementById("sidebar");
+
+    menuBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("-translate-x-full");
+    });
+
 document.addEventListener("DOMContentLoaded", () => {
     const iframe = document.getElementById("mainmenu");
 
